11d6743479380772ca0bbcf1dc6628284835132b
LittleDustDevWIN <markraph19@gmail.com>
2024-07-02 15:10:12 -0300

    feat: add SomaNumeros

--- src/list/Pesquisa/CatalogoLivros/CatalogoLivros.java
+++ src/list/Pesquisa/CatalogoLivros/CatalogoLivros.java
@@ -1,4 +1,4 @@
-package list.Pesquisa;
+package list.Pesquisa.CatalogoLivros;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/list/Pesquisa/CatalogoLivros/Livro.java
+++ src/list/Pesquisa/CatalogoLivros/Livro.java
@@ -1,4 +1,4 @@
-package list.Pesquisa;
+package list.Pesquisa.CatalogoLivros;
 
 public class Livro {
     //atributos
