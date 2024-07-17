a130b43e52c1a74b61ed8f63b045bee5a9cee1d7
LittleDustDevWIN <markraph19@gmail.com>
2024-07-17 17:52:15 -0300

    feat add: ConjuntoConvidados

---
+++ src/set/OperacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,53 @@
+package set.OperacoesBasicas;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class ConjuntoConvidados {
+    
+    //atributo
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+    
+    public void removerConvidadoPorCodigoConvite(int codigoConvite) {
+        Convidado convidadoParaRemover = null;
+        for(Convidado c : convidadoSet) {
+            if(c.getCodigoConvite() == codigoConvite) {
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados() {
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados() {
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        conjuntoConvidados.adicionarConvidado("Emanuel", 100);
+        conjuntoConvidados.adicionarConvidado("Fábio", 232);
+        conjuntoConvidados.adicionarConvidado("Gabriel", 232);
+        conjuntoConvidados.adicionarConvidado("Hélio", 364);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(100);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        conjuntoConvidados.exibirConvidados();
+    }
+}
