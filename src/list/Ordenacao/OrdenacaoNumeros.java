3a551fa4f20fff4045ffac5dfedcdc7d15db68c9
LittleDustDevWIN <markraph19@gmail.com>
2024-07-17 17:02:08 -0300

    feat add: OrdenacaoNumeros

---
+++ src/list/Ordenacao/OrdenacaoNumeros.java
@@ -0,0 +1,43 @@
+package list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoNumeros {
+    
+    private List<Integer> numeroList;
+    
+    public OrdenacaoNumeros() {
+        this.numeroList = new ArrayList();
+    }
+
+    public void adicionarNumero(int numero) {
+        this.numeroList.add(numero);
+    }
+
+    public List<Integer> ordenarAscendente() {
+        List<Integer> numeros = new ArrayList<>(numeroList);
+        Collections.sort(numeros);
+        return numeros;
+    }
+
+    public List<Integer> ordenarDescendente() {
+        List<Integer> numeros = new ArrayList<>(numeroList);
+        numeros.sort(Collections.reverseOrder());
+        return numeros;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoNumeros numeros = new OrdenacaoNumeros();
+
+        numeros.adicionarNumero(22);
+        numeros.adicionarNumero(13);
+        numeros.adicionarNumero(17);
+        numeros.adicionarNumero(51);
+
+        System.out.println(numeros.ordenarAscendente());
+        System.out.println(numeros.ordenarDescendente());
+    }
+    
+}
